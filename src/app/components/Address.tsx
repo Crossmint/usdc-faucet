1ded9bd2971ec014ccac6ae4999a50985efa6983
Panos Halios <panos@paella.dev>
2024-11-27 09:58:49 -0500

    removed error handling from address component

--- src/app/components/Address.tsx
+++ src/app/components/Address.tsx
@@ -1,36 +1,20 @@
-import { useState } from "react";
-import { useAccount } from "wagmi";
-
 interface AddressProps {
   address: string;
   setAddress: Function;
 }
 
 const Address: React.FC<AddressProps> = ({ address, setAddress }) => {
-  const [error, setError] = useState(false);
-
   return (
     <>
       <input
         type="text"
         defaultValue={address}
         onChange={(e) => {
-          const value = e.target.value;
-          if (value.startsWith("0x") && value.length === 42) {
-            setError(false);
-            setAddress(value as `0x${string}`);
-          } else {
-            setError(true);
-          }
+          setAddress(e.target.value);
         }}
-        className={`w-full text-center p-3 rounded-full font-mono text-lg bg-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600 ${
-          error ? "border border-red-500" : ""
-        }`}
+        className="w-full text-center p-3 rounded-full font-mono text-lg bg-gray-200 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-600"
         placeholder="Enter wallet address"
       />
-      {error && (
-        <span className="mt-3">You must enter a valid wallet address</span>
-      )}
     </>
   );
 };
