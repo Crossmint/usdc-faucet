85f10408c156884d61bab2062844bc1e691cfdc1
alberto-crossmint <alberto@paella.dev>
2025-11-05 13:35:32 +0100

    fmt

--- src/app/components/Address.tsx
+++ src/app/components/Address.tsx
@@ -6,113 +6,105 @@ import { useState, useEffect } from "react";
 import { StrKey } from "@stellar/stellar-base";
 
 interface AddressProps {
-  address: string;
-  chainType: ChainType;
-  onChange: (address: string) => void;
+    address: string;
+    chainType: ChainType;
+    onChange: (address: string) => void;
 }
 
 function isBase58(str: string): boolean {
-  const base58Alphabet =
-    "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
-  for (const char of str) {
-    if (!base58Alphabet.includes(char)) {
-      return false;
+    const base58Alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    for (const char of str) {
+        if (!base58Alphabet.includes(char)) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 function validateSolanaAddress(address: string): boolean {
-  // Check if the address is a string
-  if (typeof address !== "string") {
-    return false;
-  }
+    // Check if the address is a string
+    if (typeof address !== "string") {
+        return false;
+    }
 
-  // Check the length of the address
-  if (address.length !== 44) {
-    return false;
-  }
+    // Check the length of the address
+    if (address.length !== 44) {
+        return false;
+    }
 
-  // Check if the address is a valid base58 string
-  return isBase58(address);
+    // Check if the address is a valid base58 string
+    return isBase58(address);
 }
 
 function validateStellarAddress(address: string): boolean {
-  // Check if the address is a string
-  if (typeof address !== "string") {
-    return false;
-  }
+    // Check if the address is a string
+    if (typeof address !== "string") {
+        return false;
+    }
 
-  if (
-    !StrKey.isValidEd25519PublicKey(address) &&
-    !StrKey.isValidContract(address)
-  ) {
-    return false;
-  }
+    if (!StrKey.isValidEd25519PublicKey(address) && !StrKey.isValidContract(address)) {
+        return false;
+    }
 
-  return true;
+    return true;
 }
 
-export default function Address({
-  address,
-  chainType,
-  onChange,
-}: AddressProps) {
-  const [error, setError] = useState<string | null>(null);
+export default function Address({ address, chainType, onChange }: AddressProps) {
+    const [error, setError] = useState<string | null>(null);
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    setError(null);
-  }, [chainType]);
+    // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+    useEffect(() => {
+        setError(null);
+    }, [chainType]);
 
-  const validateAddress = (addr: string) => {
-    if (chainType === "evm") {
-      if (!isAddress(addr)) {
-        setError("Invalid Ethereum address");
-      } else {
-        setError(null);
-      }
-    } else if (chainType === "solana") {
-      if (!validateSolanaAddress(addr)) {
-        setError("Invalid Solana address");
-      } else {
-        setError(null);
-      }
-    } else if (chainType === "stellar") {
-      if (!validateStellarAddress(addr)) {
-        setError("Invalid Stellar address");
-      } else {
-        setError(null);
-      }
-    }
-  };
-
-  return (
-    <div className="grid w-full items-center space-y-1">
-      <Label htmlFor="address" className={error ? "text-red-500" : ""}>
-        Wallet Address
-      </Label>
-      <Input
-        type="text"
-        id="address"
-        placeholder={
-          chainType === "evm"
-            ? "Your Ethereum address"
-            : chainType === "solana"
-              ? "Your Solana address"
-              : "Your Stellar address"
+    const validateAddress = (addr: string) => {
+        if (chainType === "evm") {
+            if (!isAddress(addr)) {
+                setError("Invalid Ethereum address");
+            } else {
+                setError(null);
+            }
+        } else if (chainType === "solana") {
+            if (!validateSolanaAddress(addr)) {
+                setError("Invalid Solana address");
+            } else {
+                setError(null);
+            }
+        } else if (chainType === "stellar") {
+            if (!validateStellarAddress(addr)) {
+                setError("Invalid Stellar address");
+            } else {
+                setError(null);
+            }
         }
-        value={address}
-        className={`hover:border-[#04C768] ${error ? "border-red-500" : ""}`}
-        onChange={(e) => {
-          onChange(e.target.value);
-          setError(null);
-        }}
-        onBlur={(e) => {
-          validateAddress(e.target.value);
-        }}
-      />
-      {error && <p className="text-sm text-red-500">{error}</p>}
-    </div>
-  );
+    };
+
+    return (
+        <div className="grid w-full items-center space-y-1">
+            <Label htmlFor="address" className={error ? "text-red-500" : ""}>
+                Wallet Address
+            </Label>
+            <Input
+                type="text"
+                id="address"
+                placeholder={
+                    chainType === "evm"
+                        ? "Your Ethereum address"
+                        : chainType === "solana"
+                          ? "Your Solana address"
+                          : "Your Stellar address"
+                }
+                value={address}
+                className={`hover:border-[#04C768] ${error ? "border-red-500" : ""}`}
+                onChange={(e) => {
+                    onChange(e.target.value);
+                    setError(null);
+                }}
+                onBlur={(e) => {
+                    validateAddress(e.target.value);
+                }}
+            />
+            {error && <p className="text-sm text-red-500">{error}</p>}
+        </div>
+    );
 }
